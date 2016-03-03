dbae60b4a7661cdfba100990e7fa8947e01e74f3
Albert O'Connor <amjoconn@gmail.com>
2016-03-03 11:51:01 -0500

    Try to automatically including migrations.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
@@ -10,7 +10,7 @@ setup(
     author_email="info@albertoconnor.ca",
     url="https://github.com/AvocadoCoop/avocado-templated-mail",
     license="BSD License",
-    packages=["templated_mail"],
+    packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
     classifiers=[
